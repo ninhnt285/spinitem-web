9ad901191688c58472050b0b5e3f26785ade5ff0
nick <salem280575@gmail.com>
2018-03-27 15:22:05 -0400

    update ENV variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,11 +26,11 @@ module.exports = {
     '@nuxtjs/axios'
   ],
   axios: {
-    baseURL: process.env.NODE_END == 'production' ? 'http://api.spinitem.com' : 'http://localhost:8000'
+    baseURL: process.env.NODE_ENV == 'production' ? 'http://api.spinitem.com' : 'http://localhost:8000'
   },
   env: {
-    apiURL: process.env.NODE_END == 'production' ? 'http://api.spinitem.com' : 'http://localhost:8000',
-    clientURL: process.env.NODE_END == 'production' ? 'http://spinitem.com' : 'http://localhost:8001',
+    apiURL: process.env.NODE_ENV == 'production' ? 'http://api.spinitem.com' : 'http://localhost:8000',
+    clientURL: process.env.NODE_ENV == 'production' ? 'http://spinitem.com' : 'http://localhost:8001',
     shopifyAPIKey: 'fd28cffb2c993b304a287da9586ffd1f'
   },
   build: {
